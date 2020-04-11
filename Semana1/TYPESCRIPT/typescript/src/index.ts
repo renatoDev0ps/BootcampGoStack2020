ed7a6d88e9d401f9cbe48dc0402f114b8a48e33b
renatdev0ps <renatdevops@gmail.com>
2020-04-11 04:08:13 -0300

    Conceitos de Typescript final

--- Semana1/TYPESCRIPT/typescript/src/index.ts
+++ Semana1/TYPESCRIPT/typescript/src/index.ts
@@ -1,9 +1,8 @@
 import express from "express";
+import { helloWorld } from "./route";
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: "Hello TypeScript" });
-})
+app.get('/', helloWorld);
 
 app.listen(3333);
